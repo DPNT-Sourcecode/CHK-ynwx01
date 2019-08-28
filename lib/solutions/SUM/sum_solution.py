ec33b905771b03448ff770d79b00870cea55ac92
sbx_user1051 <sbx_user1051@169.254.6.145>
2019-08-28 18:10:43 +0000

    Wed Aug 28 18:10:43 UTC 2019

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,15 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-    raise NotImplementedError()
+    """
+    Computes the sum of two positive integers between 0 and 100.
+    :param x: A positive integer between 0 - 100
+    :param y: A positive integer between 0 - 100
+    :return: an Integer representing the sum of the two numbers
+    """
+    assert(isinstance(x, int))
+    assert(isinstance(y, int))
+    assert(0 <= x < 100)
+    assert(0 <= y < 100)
+    return x + y
+
+
