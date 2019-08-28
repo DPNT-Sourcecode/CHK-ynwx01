47be538f79eed98a937e6842477efad9cead02d2
sbx_user1051 <sbx_user1051@169.254.6.145>
2019-08-28 18:38:28 +0000

    Wed Aug 28 18:38:28 UTC 2019

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -4,8 +4,9 @@
 # friend_name = unicode string
 def hello(friend_name):
     """
-    Writes Hello to the world.
-    :param friend_name: Ignore for now
-    :return: Return a message saying hello to the world
+    Writes Hello to a friend.
+    :param friend_name: Our friend's name
+    :return: Return a message saying hello to our friend
     """
-    return "Hello, World!"
+    return "Hello, {0}!".format(friend_name)
+
