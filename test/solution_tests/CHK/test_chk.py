96a92009317b7adb672079c0d7bfdc20925bed19
sbx_user1051 <sbx_user1051@169.254.6.145>
2019-08-28 19:42:20 +0000

    Wed Aug 28 19:42:20 UTC 2019

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -3,12 +3,19 @@ from solutions.CHK import checkout_solution
 
 class TestCheckout(object):
     def test_checkout(self):
-        assert checkout_solution.checkout('ACCAABAAAACCD') == 435
-        assert checkout_solution.checkout('AAAAAAAAAA') == 440
+        assert checkout_solution.checkout('ACCAABBAAAACCD') == 425
+        assert checkout_solution.checkout('AAAAAAAAAA') == 400
+        assert checkout_solution.checkout('AAAAAAAAAAA') == 450
+        assert checkout_solution.checkout('AAAAAAAAAAAA') == 500
+        assert checkout_solution.checkout('AAAAAAAAAAAAA') == 530
+        assert checkout_solution.checkout('AAAAAAAAAAAAAA') == 580
         assert checkout_solution.checkout('BCB') == 65
+        assert checkout_solution.checkout('ECBE') == 100
+        assert checkout_solution.checkout('ECBEBBE') == 185
 
     def test_valid_skus(self):
         assert checkout_solution.checkout('ABBBFBBBCCC') == -1
         assert checkout_solution.checkout('A0B') == -1
         assert checkout_solution.checkout(0) == -1
+
 
