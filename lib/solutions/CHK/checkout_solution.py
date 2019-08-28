7150fb6e9db0436763c14f796c95c85d0328edf8
sbx_user1051 <sbx_user1051@169.254.6.145>
2019-08-28 20:39:09 +0000

    Wed Aug 28 20:39:09 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -159,12 +159,17 @@ def mix_and_match(aggregated_skus):
         sku_price_map = [(sku, PRICE_TABLE[sku]) for sku in skus]
         ordered_skus = [sku_map[0] for sku_map in sorted(sku_price_map, key=lambda tup: tup[1])]
 
-        i = 0
         # Now we can remove already mix and matched products
+        i = 0
         for n in range(quantity_items_to_remove, 0, -1):
-
+            # We go decreasingly in the least to remove the most expensive items first
+            while aggregated_skus.get(ordered_skus[i], 0):
+                aggregated_skus[ordered_skus[i]] -= 1
+            else:
+                i += 1
 
         price_mix_match += mix_and_matches * price
+
 
 
 
