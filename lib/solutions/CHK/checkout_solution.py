0a21fca392f32cb9287318bb6026c492cae927c6
sbx_user1051 <sbx_user1051@169.254.6.145>
2019-08-28 18:44:52 +0000

    Wed Aug 28 18:44:52 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,12 @@
-
+import re
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    """
+    Calculates the checkout price of a given basket.
+    :param skus: The SKUs of the products in the basket.
+    :return: The total price of the basket.
+    """
+    
+
