de114de3a1a37193e6111661018f53357c9a9ab5
sbx_user1051 <sbx_user1051@169.254.6.145>
2019-08-28 19:05:52 +0000

    Wed Aug 28 19:05:52 UTC 2019

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -1,6 +1,14 @@
 from solutions.CHK import checkout_solution
 
 
-class TestSum():
-    def test_sum(self):
-        assert sum_solution.compute(1, 2) == 3
+class TestCheckout(object):
+    def test_checkout(self):
+        assert checkout_solution.checkout('ACCAABAAAACCD') == 435
+        assert checkout_solution.checkout('AAAAAAAAAA') == 440
+        assert checkout_solution.checkout('BCB') == 110
+
+    def test_valid_skus(self):
+        assert checkout_solution.checkout('ABBBEBBBCCC') == -1
+        assert checkout_solution.checkout('A0B') == -1
+        assert checkout_solution.checkout(0) == -1
+
