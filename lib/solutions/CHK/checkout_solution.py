c0839a76c6ee1d67adfd029506d251d762f5c3e9
sbx_user1051 <sbx_user1051@169.254.6.145>
2019-08-28 18:47:52 +0000

    Wed Aug 28 18:47:52 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,11 +2,16 @@ import re
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+
+PRICE_TABLE = {'A': {'price': 50}}
+
+
 def checkout(skus):
     """
     Calculates the checkout price of a given basket.
     :param skus: The SKUs of the products in the basket.
     :return: The total price of the basket.
     """
-    
+
+
 
