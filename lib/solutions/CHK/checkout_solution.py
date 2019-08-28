2bfb807039c16fb41c8fb10774a94f106165bfea
sbx_user1051 <sbx_user1051@169.254.6.145>
2019-08-28 18:53:52 +0000

    Wed Aug 28 18:53:52 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -17,7 +17,19 @@ def checkout(skus):
     :param skus: The SKUs of the products in the basket.
     :return: The total price of the basket.
     """
-    
+    if not is_valid_skus(skus):
+        return 01
+
+def is_valid_skus(skus):
+    """
+    Checks whether the list of SKUs received is valid.
+    :param skus: A string containing the SKUs in the basket
+    :return: True is the SKUs are in a valid format, False otherwise.
+    """
+    if not isinstance(skus, str):
+        return -1
+    valid_skus = "".join(PRICE_TABLE.keys())
+    sku_regex = re.compile('^{0}$'.format())
 
 
 
